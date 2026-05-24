bdea40fda18a76072eecd5a3d9c00fa2813dfb21
AR123456 <coloradoz99@gmail.com>
2026-05-24 06:05:52 -0400

    in line style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import * as Device from "expo-device";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>My Macro Nutrient App</Text>
       <Text>Running on: {Platform.OS}</Text>
       <Text>Device Model:{Device.modelName}</Text>
@@ -14,9 +14,9 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  // container: {
+  //   flex: 1,
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
 });
