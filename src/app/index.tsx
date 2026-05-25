219d3fd32c717db5229a8ba9a34bc0591e34fea4
AR123456 <coloradoz99@gmail.com>
2026-05-25 06:44:52 -0400

    styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,21 +2,31 @@ import { Text, View, StyleSheet, Platform } from "react-native";
 import * as Device from "expo-device";
 
 export default function HomeScreen() {
+  const currentDate = new Date().toLocaleDateString();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>My Macro Nutrient App</Text>
-      <Text>Running on: {Platform.OS}</Text>
-      <Text>Device Model:{Device.modelName}</Text>
-      <Text>Device Brand:{Device.brand}</Text>
-      <Text>Device OS Version:{Device.osVersion}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Macro Nutrient App</Text>
+      <Text style={styles.date}>{currentDate} </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 28,
+    backgroundColor: "#8e8eeb",
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  date: {
+    fontSize: 14,
+    color: "#a0a0b0",
+    marginTop: 4,
+    marginBottom: 30,
+  },
 });
