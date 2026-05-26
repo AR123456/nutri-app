949554090fe9acfad7ccf0c8d9bc7e05256a9119
AR123456 <coloradoz99@gmail.com>
2026-05-26 07:38:01 -0400

    current date

--- src/app/components/HomeHeader.tsx
+++ src/app/components/HomeHeader.tsx
@@ -2,9 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const HomeHeader = () => {
+  const currentDate = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <View>
-      <Text>HomeHeader</Text>
+      <Text>{currentDate}</Text>
     </View>
   );
 };
