c31ef6e56742ec8f11551ec7812e9f92a3129efb
AR123456 <coloradoz99@gmail.com>
2026-05-28 05:09:27 -0400

    meals link

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,6 +8,9 @@ export default function HomeScreen() {
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>My Macro Nutrient App</Text>
       <HomeHeader />
+      <Link href="/meals" style={{ fontSize: 18, color: "#007bff" }}>
+        Go to Meals
+      </Link>
     </ScrollView>
   );
 }
