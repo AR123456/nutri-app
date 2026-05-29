7386a7aa0f6b550bbb512288044a2b8d6ce7e7b9
AR123456 <coloradoz99@gmail.com>
2026-05-29 06:13:16 -0400

    add meals screen

---
+++ src/app/add-meal.tsx
@@ -0,0 +1,12 @@
+import { globalStyles } from "@/styles/global";
+import { Text, View } from "react-native";
+
+const AddMealScreen = () => {
+  return (
+    <View>
+      <Text style={globalStyles.title}>AddMealScreen</Text>
+    </View>
+  );
+};
+
+export default AddMealScreen;
