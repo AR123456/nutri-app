44fc3d56c9df816a06ba041d3492e084342e595c
AR123456 <coloradoz99@gmail.com>
2026-05-30 05:12:01 -0400

    add meal

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,22 +1,41 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "@/styles/global";
 
 const TabLayout = () => {
   return (
     <View>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: colors.background,
+            borderTopColor: colors.surface,
+          },
+          tabBarActiveTintColor: colors.background,
+          tabBarInactiveTintColor: colors.textSecondary,
+        }}
+      >
         <Tabs.Screen
           name="index"
           options={{
             title: "Home",
-            tabBarIcon: ({ color, size }) => <Ionicons />,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
           }}
-        >
-          TabLayout
-        </Tabs.Screen>
-        <Tabs.Screen>TabLayout</Tabs.Screen>
-        <Tabs.Screen>TabLayout</Tabs.Screen>
+        />
+        <Tabs.Screen
+          name="add-meal"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="add-circle" size={size} color={color} />
+            ),
+          }}
+        />
       </Tabs>
     </View>
   );
