4ef2b29ca528d946c9adb2e0b8a89dd9db634aff
AR123456 <coloradoz99@gmail.com>
2026-05-31 04:50:28 -0400

    clean up root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,18 +2,8 @@ import { Stack } from "expo-router";
 import { colors } from "@/styles/global";
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.header },
-        headerTintColor: "#fff",
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false, title: "Home" }}
-      />
-      <Stack.Screen name="meals" options={{ title: "Meals" }} />
-      <Stack.Screen name="add-meal" options={{ title: "Add Meal" }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
