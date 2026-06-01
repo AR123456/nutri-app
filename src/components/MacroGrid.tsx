a52178b637e0aa42c5b0c6804e34de13dce81830
AR123456 <coloradoz99@gmail.com>
2026-06-01 09:00:00 -0400

    working on grid of macros

--- src/components/MacroGrid.tsx
+++ src/components/MacroGrid.tsx
@@ -1,13 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
-import MacroCard from "./MacroCard";
+import React from "react";
 
 const MacroGrid = () => {
   return (
-    <View style={styles.grid}>
-      <MacroCard label="Calories" value="0" goal="2,000" color="#ff6b6b" />
-      <MacroCard label="Protein" value="0g" goal="150g" color="#4ecdc4" />
-      <MacroCard label="Carbs" value="0g" goal="250g" color="#ffd93d" />
-      <MacroCard label="Fat" value="0g" goal="65g" color="#6bcb77" />
+    <View>
+      <Text>MacroGrid</Text>
     </View>
   );
 };
