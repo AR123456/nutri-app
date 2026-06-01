3587102e3d5722aed69f3ca71d410e4cb140d234
AR123456 <coloradoz99@gmail.com>
2026-06-01 09:01:07 -0400

    show macros on home screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import { globalStyles } from "@/styles/global";
-import { Link } from "expo-router";
 import { Text, ScrollView } from "react-native";
 import HomeHeader from "../../components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
@@ -8,9 +7,7 @@ export default function HomeScreen() {
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>My Macro Nutrient App</Text>
       <HomeHeader />
-      <Link href="/add-meal" style={{ fontSize: 18, color: "#007bff" }}>
-        This is add meal page
-      </Link>
+      <MacroGrid />
     </ScrollView>
   );
 }
