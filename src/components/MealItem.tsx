8494ec7aef257926bc808e98e83aaccb74ab3f41
AR123456 <coloradoz99@gmail.com>
2026-06-02 07:31:24 -0400

    custom styles

--- src/components/MealItem.tsx
+++ src/components/MealItem.tsx
@@ -20,4 +20,21 @@ const MealItem = ({ name, calories, protein, carbs, fat }: MealItemProps) => {
 
 export default MealItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#16213e",
+    borderRadius: 10,
+    padding: 16,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#ffffff",
+  },
+  macros: {
+    fontSize: 13,
+    color: "#a0a0b0",
+    marginTop: 4,
+  },
+});
