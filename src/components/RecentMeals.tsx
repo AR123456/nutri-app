0af6057b7209a075ca1fffc9290b785551967f31
AR123456 <coloradoz99@gmail.com>
2026-06-02 07:32:19 -0400

    recent meals for home

---
+++ src/components/RecentMeals.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RecentMeals = () => {
+  return (
+    <View>
+      <Text>RecentMeals</Text>
+    </View>
+  );
+};
+
+export default RecentMeals;
