6334ba229b62180931990466399c53dd2c93b35f
AR123456 <coloradoz99@gmail.com>
2026-06-03 05:34:30 -0400

    meal imports

--- src/app/(tabs)/add-meal.tsx
+++ src/app/(tabs)/add-meal.tsx
@@ -1,7 +1,21 @@
-import { globalStyles } from "@/styles/global";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { colors, globalStyles } from "@/styles/global";
 
 const AddMealScreen = () => {
+  // state management getters and setters
+  // const [name, setName] = useState("");
+  // const [calories, setCalories] = useState("");
+  // const [protein, setProtein] = useState("");
+  // const [carbs, setCarbs] = useState("");
+  // const [fat, setFat] = useState("");
+  // on button press call handleAddMeal
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Add Meal</Text>
