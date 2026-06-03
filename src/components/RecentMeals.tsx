c3a88b07312028c76a5f0afa904b494a74004d0d
AR123456 <coloradoz99@gmail.com>
2026-06-03 05:33:32 -0400

    boiler plate return

--- src/components/RecentMeals.tsx
+++ src/components/RecentMeals.tsx
@@ -4,8 +4,29 @@ import MealItem from "./MealItem";
 
 const RecentMeals = () => {
   return (
-    <View>
-      <Text>RecentMeals</Text>
+    <View style={{ marginTop: 30 }}>
+      <Text style={globalStyles.sectionTitle}>Recent Meals</Text>
+      <MealItem
+        name="Chicken & Rice"
+        calories={540}
+        protein={45}
+        carbs={50}
+        fat={12}
+      />
+      <MealItem
+        name="Protein Shake"
+        calories={280}
+        protein={30}
+        carbs={20}
+        fat={8}
+      />
+      <MealItem
+        name="Salmon Salad"
+        calories={430}
+        protein={35}
+        carbs={10}
+        fat={25}
+      />
     </View>
   );
 };
