b3f68b8aa314f5174e0cec9394682dfa00f91857
AR123456 <coloradoz99@gmail.com>
2026-06-06 06:48:54 -0400

    updated imports index

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
-import { globalStyles } from "@/styles/global";
-import { Text, ScrollView } from "react-native";
 import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
+import { getMeals, Meal } from "@/storage/meals";
+import { globalStyles } from "@/styles/global";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { ScrollView, Text } from "react-native";
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
