2c25e188042caca92467f9567091a78baee94ffa
AR123456 <coloradoz99@gmail.com>
2026-06-08 07:33:28 -0400

    showing meals

--- src/components/RecentMeals.tsx
+++ src/components/RecentMeals.tsx
@@ -9,30 +9,24 @@ const RecentMeals = ({ meals }: RecentMealsProps) => {
   return (
     <View style={{ marginTop: 30 }}>
       <Text style={globalStyles.sectionTitle}>Recent Meals</Text>
-      if (meals.length===0){" "}
-      {<Text style={globalStyles.empty}>No meals logged yet.</Text>}else
-      {<MealItem name={meals.name} />}
-      {/* <MealItem
-        name="Chicken & Rice"
-        calories={540}
-        protein={45}
-        carbs={50}
-        fat={12}
-      />
-      <MealItem
-        name="Protein Shake"
-        calories={280}
-        protein={30}
-        carbs={20}
-        fat={8}
-      />
-      <MealItem
-        name="Salmon Salad"
-        calories={430}
-        protein={35}
-        carbs={10}
-        fat={25}
-      /> */}
+      {meals.length === 0 ? (
+        <Text style={globalStyles.empty}>No meals logged yet.</Text>
+      ) : (
+        meals
+          // just first most recent meals
+          .slice(0, 5)
+          .map((meal) => (
+            <MealItem
+              // need key to show list
+              key={meal.id}
+              name={meal.name}
+              calories={meal.calories}
+              protein={meal.protein}
+              carbs={meal.carbs}
+              fat={meal.fat}
+            />
+          ))
+      )}
     </View>
   );
 };
