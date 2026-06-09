116416d9114cc0a5549a415fbb76585857b085d4
AR123456 <coloradoz99@gmail.com>
2026-06-09 06:22:57 -0400

    all to macro totals now

--- src/components/MacroGrid.tsx
+++ src/components/MacroGrid.tsx
@@ -34,9 +34,24 @@ const MacroGrid = ({ meals }: MacroGridProps) => {
         goal="2,000"
         color="#ff6b6b"
       />
-      <MacroCard label="Protein" value="0g" goal="150g" color="#4ecdc4" />
-      <MacroCard label="Carbs" value="0g" goal="250g" color="#ffd93d" />
-      <MacroCard label="Fat" value="0g" goal="65g" color="#6bcb77" />
+      <MacroCard
+        label="Protein"
+        value={`${totals.protein}g`}
+        goal="150g"
+        color="#4ecdc4"
+      />
+      <MacroCard
+        label="Carbs"
+        value={`${totals.carbs}g`}
+        goal="250g"
+        color="#ffd93d"
+      />
+      <MacroCard
+        label="Fat"
+        value={`${totals.fat}g`}
+        goal="65g"
+        color="#6bcb77"
+      />
     </View>
   );
 };
