ff27257113bc866cc9cad1f1510417558c139041
AR123456 <coloradoz99@gmail.com>
2026-06-12 07:41:49 -0400

    get meal items

--- src/app/(tabs)/meals.tsx
+++ src/app/(tabs)/meals.tsx
@@ -1,7 +1,7 @@
 import MealItem from "@/components/MealItem";
 import { getMeals, Meal } from "@/storage/meals";
 import { globalStyles } from "@/styles/global";
-import { useFocusEffect } from "expo-router";
+import { useFocusEffect, Link } from "expo-router";
 import { useCallback, useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 
@@ -16,12 +16,32 @@ const MealsScreen = () => {
       loadMeals();
     }, []),
   );
-  <ScrollView style={globalStyles.container}>
-    <Text style={globalStyles.title}>All Meals</Text>
-    <Link href="/add-meal" style={{ fontSize: 18, color: "#007bff" }}>
-      Add New Meal
-    </Link>
-  </ScrollView>;
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>All Meals</Text>
+      <View style={{ marginTop: 30 }}>
+        {meals.length === 0 ? (
+          <Text style={globalStyles.empty}>No meals logged yet</Text>
+        ) : (
+          meals.map((meal) => (
+            <MealItem
+              key={meal.id}
+              id={meal.id}
+              name={meal.name}
+              calories={meal.calories}
+              protein={meal.protein}
+              carbs={meal.carbs}
+              fat={meal.fat}
+              onDelete={loadMeals}
+            />
+          ))
+        )}
+      </View>
+      <Link href="/add-meal" style={{ fontSize: 18, color: "#007bff" }}>
+        Add New Meal
+      </Link>
+    </ScrollView>
+  );
 };
 
 export default MealsScreen;
