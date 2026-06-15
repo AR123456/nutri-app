619f8b4c58344661599c39e86f375ce9c3f4bbb4
AR123456 <coloradoz99@gmail.com>
2026-06-15 06:32:30 -0400

    share button boiler plate

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,1 +1,13 @@
 // Share API , use this button anywhere in app
+import { View, Text } from "react-native";
+import React from "react";
+
+const ShareButton = () => {
+  return (
+    <View>
+      <Text>ShareButton</Text>
+    </View>
+  );
+};
+
+export default ShareButton;
