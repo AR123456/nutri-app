90821953656c2381aaeeeb56cf494a892fb901e5
AR123456 <coloradoz99@gmail.com>
2026-06-17 06:44:04 -0400

    imports to clipboard

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,1 +1,7 @@
-// copyBUtton
+import { Ionicons } from "@expo/vector-icons";
+// getting clipboard from expo
+import * as Clipboard from "expo-clipboard";
+import * as Haptics from "expo-haptics";
+import { Alert, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Meal } from "@/storage/meals";
+import { colors } from "@/styles/global";
