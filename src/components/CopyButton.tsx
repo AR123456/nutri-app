f2d2c8362bceeb2763e1cfb97bc8f5a911e749e2
AR123456 <coloradoz99@gmail.com>
2026-06-17 06:44:39 -0400

    copy btn function

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -2,10 +2,20 @@ import { Ionicons } from "@expo/vector-icons";
 // getting clipboard from expo
 import * as Clipboard from "expo-clipboard";
 import * as Haptics from "expo-haptics";
-import { Alert, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Meal } from "@/storage/meals";
 import { colors } from "@/styles/global";
 
 type CopyButtonProps = {
   meals: Meal[];
 };
+
+const CopyButton = () => {
+  return (
+    <View>
+      <Text>CopyButton</Text>
+    </View>
+  );
+};
+
+export default CopyButton;
