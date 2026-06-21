4f5540718567e76cedef960b22f09ebda4c5f9ac
AR123456 <coloradoz99@gmail.com>
2026-06-21 05:47:01 -0400

    notifications utility

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,16 +1,1 @@
-import * as Notifications from "expo-notifications";
-//   notifications handler
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldShowBanner: true,
-    shouldShowList: true,
-    shouldShowSound: false,
-    shouldShowBadge: false,
-  }),
-});
-// request permissions
-
-// schedule mel reminders
-
-// cancel meal reminders
+// for expo notifications code
