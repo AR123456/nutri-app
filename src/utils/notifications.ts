6445ea94e0b351d9a20fc58fb1c969fa5292e2f3
AR123456 <coloradoz99@gmail.com>
2026-06-21 05:47:26 -0400

    syntax err in handler

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,1 +1,16 @@
-// for expo notifications code
+import * as Notifications from "expo-notifications";
+//   notifications handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldShowSound: false,
+    shouldShowBadge: false,
+  }),
+});
+// request permissions
+
+// schedule mel reminders
+
+// cancel meal reminders
