ab2b209cc9855ab87ae8a6df8cb49c0fcbe003db
AR123456 <coloradoz99@gmail.com>
2026-06-23 06:08:58 -0400

    toggle

--- src/components/ReminderToggle.tsx
+++ src/components/ReminderToggle.tsx
@@ -1,1 +1,15 @@
-// reminder
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ReminderToggle = () => {
+  // getter setter
+  return (
+    <View>
+      <Text>ReminderToggle</Text>
+    </View>
+  );
+};
+
+export default ReminderToggle;
+
+const styles = StyleSheet.create({});
