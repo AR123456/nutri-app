ac84057e473e0c0b9b915db2a9b82f6df1768566
AR123456 <coloradoz99@gmail.com>
2026-06-23 06:09:17 -0400

    imports

--- src/components/ReminderToggle.tsx
+++ src/components/ReminderToggle.tsx
@@ -1,5 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { colors } from "@/styles/global";
+import {
+  cancelMealReminders,
+  requestPermissions,
+  scheduleMealReminders,
+} from "@/utils/notifications";
 
 const ReminderToggle = () => {
   // getter setter
