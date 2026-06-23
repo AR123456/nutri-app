c419c091384c37c4b9dd6438a1f39f6a34f13365
AR123456 <coloradoz99@gmail.com>
2026-06-23 06:10:38 -0400

    toggle correct syntax

--- src/components/ReminderToggle.tsx
+++ src/components/ReminderToggle.tsx
@@ -21,15 +21,13 @@ const ReminderToggle = () => {
   const toggle = async (value: boolean) => {
     if (value) {
       const granted = await requestPermissions();
-      if (!granted) {
-        return;
-        await scheduleMealReminders();
-      } else {
-        await cancelMealReminders();
-      }
-      setEnabled(value);
-      await AysncStorage.setItem(REMINDERS_KEY, value.toString());
+      if (!granted) return;
+      await scheduleMealReminders();
+    } else {
+      await cancelMealReminders();
     }
+    setEnabled(value);
+    await AsyncStorage.setItem(REMINDERS_KEY, value.toString());
   };
   return (
     <View>
