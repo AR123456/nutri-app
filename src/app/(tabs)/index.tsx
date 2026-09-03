4b0e2ef2332af94ac98a18922424cddd0a1877b7
AR123456 <coloradoz99@gmail.com>
2026-09-03 07:14:37 -0400

    adding CRUD for update

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,41 +1,39 @@
+import CopyButton from "@/components/CopyButton";
 import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
+import ReminderToggle from "@/components/ReminderToggle";
+import ShareButton from "@/components/ShareButton";
 import { getMeals, Meal } from "@/storage/meals";
 import { globalStyles } from "@/styles/global";
 import { useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
-import { ScrollView, Text, View, Platform } from "react-native";
-import ShareButton from "@/components/ShareButton";
-import CopyButton from "@/components/CopyButton";
-import ReminderToggle from "@/components/ReminderToggle";
+import { Platform, ScrollView, Text, View } from "react-native";
+
 export default function HomeScreen() {
-  // set meals state to empty array
   const [meals, setMeals] = useState<Meal[]>([]);
-  // use getMeals meal handler to get meals from local storage
+
   const loadMeals = async () => {
     const data = await getMeals();
     setMeals(data);
-    console.log("Loaded meals", data);
   };
-  //load the meals whenever the home screen comes into focus
+
   useFocusEffect(
     useCallback(() => {
       loadMeals();
     }, []),
   );
+
   return (
     <ScrollView style={globalStyles.container}>
       <View style={globalStyles.header}>
-        <Text style={globalStyles.title}>My Macro Nutrient App</Text>
+        <Text style={globalStyles.title}>MacroZone</Text>
         <ShareButton meals={meals} />
       </View>
       <HomeHeader />
       <MacroGrid meals={meals} />
       <CopyButton meals={meals} />
-      {/* <ReminderToggle /> */}
-
-      {Platform.OS !== "android" && <ReminderToggle />}
+      {/* {Platform.OS !== "android" && <ReminderToggle />} */}
       <RecentMeals meals={meals} onDelete={loadMeals} />
     </ScrollView>
   );
